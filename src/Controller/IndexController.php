7bef2446b036f6d0f0460313f8490a610686c2bb
oumniamimounii <oumniamimouni04@gmail.com>
2025-11-01 12:42:08 +0100

    routing

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,9 +15,15 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
-    #[Route('/index',name: 'app_index')]
-    public function Welcome():Response
+    // #[Route('/index',name: 'app_index')]
+    // public function Welcome():Response
+    // {
+    //     return new Response("Hello World!");
+    // }
+
+     #[Route('/index/{smia}/{age}', name: 'app_home', requirements:['smia'=>'[a-zA-Z]+','age'=>'\d+'])]
+    public function home(string $smia ,int $age ): Response
     {
-        return new Response("Hello World!");
+        return new Response("hello ".$smia. " your age is " .$age." !");
     }
 }
